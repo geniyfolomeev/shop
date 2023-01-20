1488d3bd9cb99331908642fcf93eeec5b16fa70f
Evgeniy Folomeev <e.folomeev@vk.team>
2023-01-21 01:39:03 +0300

    SUPERFIX

--- user.go
+++ user.go
@@ -2,8 +2,8 @@ package shop
 
 type User struct {
 	Id       int    `json:"-" db:"id"`
-	Login    string `json:"login" binding:"required"`
-	Name     string `json:"name" binding:"required"`
-	Surname  string `json:"surname" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Login    string `json:"login" binding:"required" db:"login"`
+	Name     string `json:"name" binding:"required" db:"first_name"`
+	Surname  string `json:"surname" binding:"required" db:"surname"`
+	Password string `json:"password" binding:"required" db:"password_hash"`
 }
