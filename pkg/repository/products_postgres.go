97d0deaee95fc6f1f7bda19b7b11353b0fad9957
Evgeniy Folomeev <e.folomeev@vk.team>
2023-01-20 23:42:19 +0300

    переделал в base64

--- pkg/repository/products_postgres.go
+++ pkg/repository/products_postgres.go
@@ -18,7 +18,7 @@ func (r *ProductsListPostgres) CreateProduct(product shop.Product) (int, error) 
 	var id int
 	query := fmt.Sprintf(
 		"INSERT INTO %s (title, price, description, category, image, rating) "+
-			"values ($1, $2, $3, $4, $5, $6) RETURNING id", productsTable)
+			"values ($1, $2, $3, $4, decode($5, 'base64'), $6) RETURNING id", productsTable)
 	row := r.db.QueryRow(query, product.Title, product.Price, product.Description, product.Category, product.Image, product.Rating)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
@@ -29,14 +29,14 @@ func (r *ProductsListPostgres) CreateProduct(product shop.Product) (int, error) 
 
 func (r *ProductsListPostgres) GetAllProducts() ([]shop.Product, error) {
 	var products []shop.Product
-	query := fmt.Sprintf("SELECT * FROM %s", productsTable)
+	query := fmt.Sprintf("SELECT title, price, description, category, encode(image, 'base64') as image, rating FROM %s", productsTable)
 	err := r.db.Select(&products, query)
 	return products, err
 }
 
 func (r *ProductsListPostgres) GetProductById(id int) (shop.Product, error) {
 	var product shop.Product
-	query := fmt.Sprintf("SELECT * FROM %s WHERE id=$1", productsTable)
+	query := fmt.Sprintf("SELECT title, price, description, category, encode(image, 'base64') as image, rating FROM %s WHERE id=$1", productsTable)
 	err := r.db.Get(&product, query, id)
 	return product, err
 }
